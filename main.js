381afc8fba084ee65a6be988e58401a432e6a93c
giorgi <127527335+nozadze22@users.noreply.github.com>
2024-02-07 16:09:21 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const main = document.querySelector("#main");
+const catchme = document.querySelector("#reject");
+const love = document.querySelector("#accept");
+const front = document.querySelector("#front");
+const back = document.querySelector("#back");
+const afro = document.querySelector("#afro");
+
+catchme.addEventListener("mouseover", ()=>{
+mouseHover();
+
+});
+function mouseHover () {
+    const i = Math.floor(Math.random()*500)+1;
+    const j = Math.floor(Math.random()*300)+1;
+    
+
+catchme.style.left = i+ "px";
+catchme.style.top = j+ "px";
+
+}
+
+
+
+love.addEventListener("click",() => {
+front.style.display = "none";
+back.style.display ="block";
+afro.style.display = 'block';
+
+});
